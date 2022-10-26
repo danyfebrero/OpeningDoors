2c9eb0691b13c59e65aa92cb7e42701e552587c4
Daniel Jimenez <danyfebrero@gmail.com>
2022-10-25 23:58:42 -0400

    update in the create key  method

--- api_key.py
+++ api_key.py
@@ -10,10 +10,9 @@ import os
 from os.path import exists
 from os import path
 from dotenv import load_dotenv
-from dotenv import dotenv_values
+from dotenv import set_key
 
-
-def configure():
+def update_env():
     load_dotenv()
 
 def key_exist():
@@ -26,22 +25,21 @@ def create_key(key_input):
     """ 
         Does: creates a file with the api key from the user input
     """
-    with open(".env","w") as file:
-        key =f"API_KEY={key_input}"
-        file.write(key)
+    set_key('.env','API_KEY',key_input)
+    update_env()
+    
 
 def get_key():
     """ 
         Does: if the key is saved loads the key if not then ask for one
         Return: the key from the file or the new one
     """
-
     while not key_exist():
         key_input = input("Please introduce your Realty Mole API key (if you don't have a key please create one for free at https://rapidapi.com/realtymole/api/realty-mole-property-api: ")
         if len(key_input) == 0:
             continue
         create_key(key_input)
-    configure()
+    update_env()
     return os.getenv('API_KEY')
 
 if __name__ == "__main__":
