cc4284f2e73fab1b1df0fa9818e0e9ec71771d81
Daniel Jimenez <danyfebrero@gmail.com>
2022-10-26 15:58:21 -0400

    create url function in model

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ This project will allow the user to search your home value, and analyze selling 
 ## Requirements:
 
   * Python 3.9
+  * python-dotenv
+  * os
+  * requests
   * Streamlit
   * Pandas
   * Realty Mole Property API key (gives 50 Requests per month in the free suscription)
--- model.py
+++ model.py
@@ -6,6 +6,11 @@ Created on Fri Oct 21 23:22:00 2022
 @author: Daniel Jimenez
 """
 
+import requests
+
+from api_key import get_key
+
+
 def main():
     """ 
         Does: xxx
@@ -14,5 +19,41 @@ def main():
     """
     pass
 
+def create_url(endpoint, address, city, state, zipcode):
+    """
+        Does: takes a full address and makes a resquest url\n
+        Arguments:\n
+            endpoints : string ['properties', 'salePrice', 'rentalPrice', 'saleListings', 'rentalListings']\n
+            The property address, in the format of 'Street, City, State, Zip'.\n
+            propertyType : string ['Single Family', 'Condo', 'Townhouse', 'Duplex-Triplex', 'Apartment']\n
+            daysOld: integer (The maximum number of days since comparable listings were last seen on the market, with a minimum of 15)\n
+            compCount : integer (The number of comparable listings returned by the API, between 5 and 20. Defaults to 5 if not provided)\n
+            limit : integer (The maximum number of listings to return, between 1 and 50. Defaults to 50 if not provided)\n
+        Return: resquest url based in the desired endpoint
+    """
+    url = f"https://realty-mole-property-api.p.rapidapi.com/{endpoint}"
+
+    # parameters for value estimate and rent estimate
+    # "propertyType":"Single Family","bedrooms":"4","bathrooms":"2","squareFootage":"1600","compCount":"5"
+    # parameters for sale listing and rental listing
+    # "city":"Austin","state":"TX","limit":"10"
+
+    if endpoint == 'properties':
+        querystring = {'address':f'{address}, {city}, {state}, {zipcode}'}
+    elif endpoint == 'salePrice' or endpoint == 'rentalPrice':
+        querystring = {'address':f'{address}, {city}, {state}, {zipcode}',"propertyType":{}, "bedrooms":{},"bathrooms":{},"squareFootage":{},"daysOld":{},"compCount":{}}
+    elif endpoint == 'saleListings' or endpoint == 'rentalListings':
+        querystring = {"city":{city},"state":{state},"limit":{}}
+
+    headers = {
+	    "X-RapidAPI-Key": get_key(),
+	    "X-RapidAPI-Host": "realty-mole-property-api.p.rapidapi.com"
+        }
+
+    request_url = f'"GET", {url}, headers={headers}, params={querystring}'
+    #response = requests.request("GET", url, headers=headers, params=querystring)
+    return request_url
+
 if __name__ == "__main__":
-    main()
+    print(create_url("properties", "5500 Grand Lake Dr", "San Antonio", "TX", 78244))
+    
